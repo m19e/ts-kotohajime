ed49ad3d622cb2561ca16d19ac99cdefdb15cab5
m19e <nexus.me7290@gmail.com>
2020-06-08 00:34:03 +0900

    Add List component

--- src/App.tsx
+++ src/App.tsx
@@ -24,4 +24,30 @@ const Reverse: React.FC = (): JSX.Element => {
     );
 };
 
+interface IItem {
+    id: number;
+    name: string;
+}
+
+const List: React.FC = (): JSX.Element => {
+    const [text, setText] = React.useState<string>("");
+    const [items, setItems] = React.useState<IItem[]>([]);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setItems([...items, { id: items.length + 1, name: text }]);
+        setText("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
+            <button>Add</button>
+            {items.map((item: IItem) => (
+                <p>{item.name}</p>
+            ))}
+        </form>
+    );
+};
+
 export default App;
