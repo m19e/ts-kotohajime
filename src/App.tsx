62380925e06cb255414a1acee9b4a60f8853a2dc
m19e <nexus.me7290@gmail.com>
2020-06-07 22:03:42 +0900

    Add Reverse component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 type AppProps = { message: string };
 
-const App: React.FunctionComponent<{ message: string }> = ({ message }) => {
-    return <div>{message}</div>
+const App: React.FC<{ message: string }> = ({ message }) => {
+    return (
+        <>
+            <div>{message}</div>
+            <Reverse />
+        </>
+    );
 };
 
+const Reverse: React.FC = (): JSX.Element => {
+    const [text, setText] = useState("");
 
+    return (
+        <>
+            <p>text: {text.split("").reverse().join("")}</p>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
+        </>
+    );
+};
 
 export default App;
